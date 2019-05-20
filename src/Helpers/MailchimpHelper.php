a9158705a14835b4f6ab0802f08e250b6a1da785
James Heathcote <james.h@biffbangpow.com>
2019-05-20 16:51:24 +0100

    Subscribe to lists works

--- src/Helpers/MailchimpHelper.php
+++ src/Helpers/MailchimpHelper.php
@@ -5,6 +5,10 @@ use SilverStripe\SiteConfig\SiteConfig;
 
 class MailchimpHelper
 {
+    private $defaultListID;
+
+    private $mailChimp;
+
     /**
      * MailchimpHelper constructor.
      * @throws Exception
@@ -13,16 +17,50 @@ class MailchimpHelper
     {
         $siteConfig = SiteConfig::current_site_config();
         $apiKey = $siteConfig->MailchimpAPIKey;
+        $defaultListID = $siteConfig->MailchimpSubscribeListID;
 
-        if ($apiKey === '' || $apiKey === null) {
+        if (
+            $apiKey === '' ||
+            $apiKey === null
+        ) {
             throw new Exception('You must enter an API key in Site Config to use this class');
         }
 
-        $mailChimp = new MailChimp($apiKey);
+        if (
+            $defaultListID === '' ||
+            $defaultListID === null
+        ) {
+            throw new Exception('You must enter a default list ID in Site Config to use this class');
+        }
 
-        $result = $mailChimp->get('lists');
-        print_r($result);
-        var_dump($result);
-        die();
+        $this->defaultListID = $defaultListID;
+
+        $this->mailChimp = new MailChimp($apiKey);
+    }
+
+    /**
+     * @param string $emailAddress
+     * @param string|null $listID
+     * @return bool
+     */
+    public function subscribeUserToList(string $emailAddress, string $listID = null)
+    {
+        $listID = $listID ?? $this->defaultListID;
+        $result = $this->mailChimp->post("lists/$listID/members", [
+            'email_address' => $emailAddress,
+            'status'        => 'subscribed',
+        ]);
+
+        $status = $result['status'];
+
+        switch ($status) {
+            case 200:
+            case 400:
+                return true;
+                break;
+            default:
+                return false;
+                break;
+        }
     }
 }
