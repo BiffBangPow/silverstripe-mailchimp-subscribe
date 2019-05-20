c9889458cc279b581e9a0507e0a365a33191e801
James Heathcote <james.h@biffbangpow.com>
2019-05-20 16:33:22 +0100

    Mailchimp helper made

--- src/Helpers/MailchimpHelper.php
+++ src/Helpers/MailchimpHelper.php
@@ -1,6 +1,28 @@
 <?php
 
+use DrewM\MailChimp\MailChimp;
+use SilverStripe\SiteConfig\SiteConfig;
+
 class MailchimpHelper
 {
+    /**
+     * MailchimpHelper constructor.
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        $siteConfig = SiteConfig::current_site_config();
+        $apiKey = $siteConfig->MailchimpAPIKey;
 
+        if ($apiKey === '' || $apiKey === null) {
+            throw new Exception('You must enter an API key in Site Config to use this class');
+        }
+
+        $mailChimp = new MailChimp($apiKey);
+
+        $result = $mailChimp->get('lists');
+        print_r($result);
+        var_dump($result);
+        die();
+    }
 }
