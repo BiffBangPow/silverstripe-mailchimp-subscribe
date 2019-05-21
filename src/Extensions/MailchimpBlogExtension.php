e057333bec707f9a98fb0386a7e7024ae87a85aa
James Heathcote <james.h@biffbangpow.com>
2019-05-21 15:00:16 +0100

    Removed dead code

--- src/Extensions/MailchimpBlogExtension.php
+++ src/Extensions/MailchimpBlogExtension.php
@@ -2,20 +2,9 @@
 
 namespace BiffBangPow\SilverStripeMailchimpSubscribe\Extensions;
 
-use BiffBangPow\SilverStripeMailchimpSubscribe\Actions\GridFieldBlogPostMailoutAction;
-use SilverStripe\Forms\FieldList;
 use SilverStripe\ORM\DataExtension;
 
 class MailchimpBlogExtension extends DataExtension
 {
-    /**
-     * @param FieldList $fields
-     * @return void
-     */
-    public function updateCMSFields(FieldList $fields)
-    {
-        $childPages = $fields->fieldByName('Root.ChildPages');
-        $childPagesGrid = $childPages->Fields()->fieldByName('ChildPages');
-        $childPagesGrid->getConfig()->addComponent(new GridFieldBlogPostMailoutAction());
-    }
+
 }
