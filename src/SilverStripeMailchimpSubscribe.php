bf5f2a12a9dcbb44119dc3db06d03e4d8db54bea
James Heathcote <james.h@biffbangpow.com>
2019-05-21 12:16:18 +0100

    Defaults in PHP class instead of yml

--- src/SilverStripeMailchimpSubscribe.php
+++ src/SilverStripeMailchimpSubscribe.php
@@ -7,4 +7,14 @@ use SilverStripe\Core\Config\Configurable;
 class SilverStripeMailchimpSubscribe
 {
     use Configurable;
+
+    /**
+     * @config
+     */
+    private static $email_field_class = 'col-12';
+
+    /**
+     * @config
+     */
+    private static $submit_button_class = 'btn btn-primary';
 }
