2389ae20de8f4d232ae021865d88e2fba6545735
James Heathcote <james.h@biffbangpow.com>
2019-05-22 09:42:36 +0100

    Added more styles to site config

--- src/Extensions/MailchimpSiteConfigExtension.php
+++ src/Extensions/MailchimpSiteConfigExtension.php
@@ -12,8 +12,15 @@ class MailchimpSiteConfigExtension extends DataExtension
      * @var array
      */
     private static $db = [
-        'MailchimpAPIKey'          => 'Varchar(200)',
-        'MailchimpSubscribeListID' => 'Varchar(200)',
+        'MailchimpAPIKey'                        => 'Varchar(200)',
+        'MailchimpSubscribeListID'               => 'Varchar(200)',
+        'MailchimpMailoutHeaderBackgroundColour' => 'Varchar(200)',
+        'MailchimpMailoutHeaderTextColour'       => 'Varchar(200)',
+        'MailchimpMailoutBodyBackgroundColour'   => 'Varchar(200)',
+        'MailchimpMailoutBodyTextColour'         => 'Varchar(200)',
+        'MailchimpMailoutButtonBackgroundColour' => 'Varchar(200)',
+        'MailchimpMailoutButtonTextColour'       => 'Varchar(200)',
+        'MailchimpMailoutButtonBorderRadius'     => 'Varchar(200)',
     ];
 
     /**
@@ -25,6 +32,13 @@ class MailchimpSiteConfigExtension extends DataExtension
         $fields->addFieldsToTab('Root.Mailchimp', [
             TextField::create('MailchimpAPIKey', 'Mailchimp API Key'),
             TextField::create('MailchimpSubscribeListID', 'Mailchimp Subscribe List ID'),
+            TextField::create('MailchimpMailoutHeaderBackgroundColour'),
+            TextField::create('MailchimpMailoutHeaderTextColour'),
+            TextField::create('MailchimpMailoutBodyBackgroundColour'),
+            TextField::create('MailchimpMailoutBodyTextColour'),
+            TextField::create('MailchimpMailoutButtonBackgroundColour'),
+            TextField::create('MailchimpMailoutButtonTextColour'),
+            TextField::create('MailchimpMailoutButtonBorderRadius'),
         ]);
     }
 }
